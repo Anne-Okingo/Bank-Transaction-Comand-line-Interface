3087cb14b18fc5836cdabadbe040022cdc277525
aokingo <annekadiso@gmail.com>
2024-09-18 15:30:30 +0300

    test: add unit test for credit function

---
+++ Aokingo/functions/credit_test.go
@@ -0,0 +1,15 @@
+package functions
+
+import(
+"testing"
+)
+
+func TestCredit(t *testing.T){
+	input1,input2 := 1000, 100
+	want := 900
+	got := Credit(input1,input2)
+
+	if want != got{
+		t.Errorf("Test Debit Failled : want %d, got %d", want, got)
+	}
+}
